0ba5693c87e100b48b6f73c3989afec1a82ee1ea
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-04-21 15:27:26 -0400

    update

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -126,6 +126,28 @@ with tf.Session() as sess:
 	plt.legend(loc='upper left')
 	plt.show()
 
+	# plot another graph that animation of how gradient descent sequentially adjusted size_factor and price_offet to
+	# find the values that returned the "best" fit line
+	fig, ax = plt.subplots()
+	line, = ax.plot(house_size, house_price)
+
+	plt.rcParams["figure.figsize"] = (10,8)
+	plt.figure()
+	plt.ylabel("Price")
+	plt.xlabel("Size (sq.ft)")
+	plt.plot(train_house_size, train_price, 'go', label='Training data')
+	plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
+
+	def animate(i):
+		line.set_xdata(train_house_size_norm * train_house_size_std + train_house_size_mean) # update the data
+		line.set_ydata((fit_size_factor[i] * train_house_size_norm + fit_price_offsets[i]) * train_price_std + train_price_mean)
+		return line,
+
+	# init only required for blitting to givea clean slate
+	def initAnim():
+		line.set_ydata(np.zeros(shape=house_price.shape[0])) # set y's to 0
+		return line,
+
 
 
 
