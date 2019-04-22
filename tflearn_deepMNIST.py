db9f16a16d247ffb71eeb9c165ccec6fa49d29cf
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-04-22 15:41:48 -0400

    update

--- tflearn_deepMNIST.py
+++ tflearn_deepMNIST.py
@@ -54,4 +54,4 @@ num_epoch = 2 # number of times through the data
 model = tflearn.DNN(network, tensorboard_verbose=0) # for more info in tensorboard turn on tensorboard_verbose
 model.fit({'input': train_images}, {'target': mnist.train.labels}, n_epoch=num_epoch,
 		validation_set=({'input': test_images}, {'target': mnist.test.labels}),
-		show metric=True, run_id='TFLearn_DeepMNIST')
+		show_metric=True, run_id='TFLearn_DeepMNIST')
