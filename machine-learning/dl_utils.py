934aec0ee3327a1e83416987b4d9707641561ff9
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-08-06 15:49:54 -0400

    update

--- machine-learning/dl_utils.py
+++ machine-learning/dl_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import h5py
+import matplotlib.pyplot as plt
 
 
 def load_dataset(train_set_location, test_set_location):
@@ -58,6 +59,23 @@ def load_planar_dataset():
     return X, Y
 
 
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding.
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them.
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid.
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples.
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+
+
 def sigmoid(z):
     """
     Compute the sigmoid of z.
@@ -70,6 +88,21 @@ def sigmoid(z):
     """
 
     s = 1 / (1 + np.exp(-z))
+    
+    return s
+
+
+def relu(x):
+    """
+    Compute the relu of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- relu(x)
+    """
+    s = np.maximum(0,x)
     
     return s
 
@@ -94,20 +127,3 @@ def initialize_with_zeros(dim):
     assert(isinstance(b, float) or isinstance(b, int))
     
     return w, b
-
-
-def plot_decision_boundary(model, X, y):
-    # Set min and max values and give it some padding.
-    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
-    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
-    h = 0.01
-    # Generate a grid of points with distance h between them.
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    # Predict the function value for the whole grid.
-    Z = model(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    # Plot the contour and training examples.
-    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
-    plt.ylabel('x2')
-    plt.xlabel('x1')
-    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
