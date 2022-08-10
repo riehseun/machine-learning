b873e0095432dbc5d4b3190a1b74a10904ab57af
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-08-10 18:42:41 -0400

    update

--- machine-learning/cv/cv_utils.py
+++ machine-learning/cv/cv_utils.py
@@ -22,3 +22,13 @@ def load_dataset():
 def convert_to_one_hot(Y, C):
     Y = np.eye(C)[Y.reshape(-1)].T
     return Y
+
+
+def scale_boxes(boxes, image_shape):
+    """ Scales the predicted boxes in order to be drawable on the image"""
+    height = image_shape[0]
+    width = image_shape[1]
+    image_dims = K.stack([height, width, height, width])
+    image_dims = K.reshape(image_dims, [1, 4])
+    boxes = boxes * image_dims
+    return boxes
