1f30f0cf94970f993092faf68ad881222df0872d
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-08-16 14:45:26 -0400

    update

--- machine-learning/dl/dl_utils.py
+++ machine-learning/dl/dl_utils.py
@@ -73,15 +73,15 @@ def load_2D_dataset():
     return train_X, train_Y, test_X, test_Y
 
 
-def load_dataset():
-    np.random.seed(3)
-    train_X, train_Y = sklearn.datasets.make_moons(n_samples=300, noise=.2) #300 #0.2 
-    # Visualize the data
-    plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
-    train_X = train_X.T
-    train_Y = train_Y.reshape((1, train_Y.shape[0]))
+# def load_dataset():
+#     np.random.seed(3)
+#     train_X, train_Y = sklearn.datasets.make_moons(n_samples=300, noise=.2) #300 #0.2 
+#     # Visualize the data
+#     plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+#     train_X = train_X.T
+#     train_Y = train_Y.reshape((1, train_Y.shape[0]))
     
-    return train_X, train_Y
+#     return train_X, train_Y
 
 
 def plot_decision_boundary(model, X, y):
