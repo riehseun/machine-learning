8e1d297fd4267d346fc633227f528030ca21138f
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-08-17 18:16:33 -0400

    update

--- machine-learning/nlp/nlp_utils.py
+++ machine-learning/nlp/nlp_utils.py
@@ -23,3 +23,45 @@ def read_glove_vecs(glove_file):
             word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
             
     return words, word_to_vec_map
+
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+        
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+
+def read_csv(filename):
+    phrase = []
+    emoji = []
+
+    with open (filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+
+        for row in csvReader:
+            phrase.append(row[0])
+            emoji.append(row[1])
+
+    X = np.asarray(phrase)
+    Y = np.asarray(emoji, dtype=int)
+
+    return X, Y
+
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
