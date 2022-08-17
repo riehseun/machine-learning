373f5eda25480960625cf6b127ea69d328a8f5a4
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-08-17 14:32:54 -0400

    update

--- machine-learning/nlp/nlp_utils.py
+++ machine-learning/nlp/nlp_utils.py
@@ -8,3 +8,18 @@ def softmax(x):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+# Word vector
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+            
+    return words, word_to_vec_map
